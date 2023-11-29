13618d3b3846b231dd85d0ce4a48e253e93e58b1
oggnimodd <arnold.ds.dev@gmail.com>
2023-11-29 11:03:16 +0700

    add LoadingIndicator

---
+++ apps/expo-app/src/components/LoadingIndicator.tsx
@@ -0,0 +1,22 @@
+import { View, Dimensions } from "react-native";
+import React from "react";
+import * as Progress from "react-native-progress";
+import tw from "twrnc";
+import { colors } from "@/tailwind";
+
+const { width, height } = Dimensions.get("window");
+
+const Loading: React.FC = () => {
+  return (
+    <View
+      style={tw.style("absolute flex-row justify-center items-center", {
+        width,
+        height,
+      })}
+    >
+      <Progress.CircleSnail thickness={5} size={100} color={colors.blue[500]} />
+    </View>
+  );
+};
+
+export default Loading;
--- apps/expo-app/src/screens/WelcomeScreen.tsx
+++ apps/expo-app/src/screens/WelcomeScreen.tsx
@@ -3,16 +3,14 @@ import tw from "twrnc";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from ".";
 import { FC } from "react";
-import { getPersonMovies } from "@/api";
-
-getPersonMovies(33934);
+import { LoadingIndicator } from "@/components";
 
 type Props = NativeStackScreenProps<RootStackParamList, "WelcomeScreen">;
 
 const WelcomeScreen: FC<Props> = ({ navigation }) => {
   return (
     <View>
-      <Text>Hello world</Text>
+      <LoadingIndicator />
     </View>
   );
 };
